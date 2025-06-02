8e8a8d77c3a7862bcc85abf68c421f060e9701b2
Beste <bestegursoy52@hotmail.com>
2025-06-02 21:56:12 +0300

    ..

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts', # bura çalışmazsa app'li versiyonu kullan
-    'rest_framework', # not sure
+    'posts',
+    'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
+}
